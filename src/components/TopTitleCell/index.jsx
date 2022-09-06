8487500d4ad905de7ce4ed7dc83b06d60bb886fd
kaiyali <kaiyali@microsoft.com>
2022-09-06 19:42:42 +0800

    try

--- src/components/TopTitleCell/index.jsx
+++ src/components/TopTitleCell/index.jsx
@@ -19,28 +19,21 @@ const TopTitleCell = ({OriginHeadline, Headline, PVLift, OriginCTR, OptimaizedCT
                 <div className="DataAreaHalf">
                     <div className="OriTitle">Original headlines</div>
                     <div className="OriTitleContent" title={OriginHeadline}>{OriginHeadline}</div>
-
                 </div>
                 <div className="DataAreaRight">
                     <div className="DataCell ">
                         <div className="DataCellTitle">PV lift</div>
                         <div className="DataCellContent GrowthData">{Triangle()}{`${parseFloat(PVLift * 100).toPrecision(3)}%`}</div>
-
                     </div>
                     <div className="DataCell">
                         <div className="DataCellTitle">Original CTR</div>
                         <div className="DataCellContent">{`${parseFloat(OriginCTR * 100).toPrecision(2)}%`}</div>
-
-
                     </div>
                     <div className="DataCell">
                         <div className="DataCellTitle">Optimized CTR</div>
                         <div className="DataCellContent" >{`${parseFloat(OptimaizedCTR * 100).toPrecision(2)}%`}</div>
                     </div>
-
                 </div>
-
-
             </div>
         </div>
         <Image
