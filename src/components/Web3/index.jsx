87c888663b8db98345313aad588c34c69c00f7d6
wanghao <wanghao090906@gmail.com>
2022-09-08 11:16:04 +0800

    feat: modify web3
    Signed-off-by: wanghao <wanghao090906@gmail.com>

--- src/components/Web3/index.jsx
+++ src/components/Web3/index.jsx
@@ -4,6 +4,7 @@ import { getTopHeadlines } from "../../utils"
 import { Logo } from '../../containers/assets';
 import liIcon from '../../images/li-icon.png';
 import content1 from '../../images/content-1.png';
+import content2 from '../../images/content-2.png';
 const testData = {
         OriginHeadline: "Works worth millions donated to gallery in show of support", Headline: "Machine Gun Kelly and Mod Sun's Stoner Comedy Was Inspired by a Real-Life Event", PVLift: "21.5%", OriginCTR: "1.5%", OptimaizedCTR:"9.7%", ImgUrl: "https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
 }
@@ -36,7 +37,7 @@ const Web3 = () => {
         </div>
       </header>
       <div className="TopsCells">
-          <div className="TopAreaContainer">
+          <div className="TopAreaContainerWeb3">
               <div className="TopTitle">Your Web3 journey begins here</div>
               <div className="TopSubTitle">Onboarding creators to Web3 world seamlessly & easily</div>
           </div>
@@ -51,7 +52,15 @@ const Web3 = () => {
                     <img src={content1} alt='content-1' />
                 </div>
             </div>
-
+            <div className='itemContainer'>
+            <div className='web3ImgContainer'>
+                    <img src={content2} alt='content-1' />
+                </div>
+                <div className="title">
+                    <p className="mainTitle">Easily access Web3 without crypto experience</p>
+                    <p className="subTitle"><img src={ liIcon } alt='li-icon' /> Simple login and interact. No technical barriers</p>
+                </div>
+            </div>
           </div>
 
       </div>
